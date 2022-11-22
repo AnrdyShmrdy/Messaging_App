f8397f53a17f35270bb275dfd0a11502a4bbc781
Andy Ponce <andy.ponce@outlook.com>
2022-11-22 17:35:30 -0600

    Removed unnecessary line

--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -5,7 +5,6 @@ public partial class HomePage : ContentPage
 	public HomePage()
 	{
 		InitializeComponent();
-        BindingContext = new Models.Home();
         navButton.Clicked += async (sender, args) =>
         {
             await Navigation.PushAsync(new ClientPage());
